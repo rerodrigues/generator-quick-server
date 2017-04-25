7d299f7ede36669578c7621a3070b4e35d6faec0
Renato Rodrigues <renato.sp@gmail.com>
2017-04-25 18:59:08 -0300

    Using grunt-localtunnel-client instead of grunt-localtunnel-me
    grunt-localtunnel-me was causing a lot of problems, ranging from unmet
    dependencies to memory leaks.

--- generators/app/templates/_tpl/Gruntfile.js
+++ generators/app/templates/_tpl/Gruntfile.js
@@ -3,8 +3,8 @@ module.exports = function(grunt) {
 
     grunt.registerTask('compile', ['sass:app', 'babel:app', 'uglify:app', 'clean:tmp']);
 <% if(expose) { %>
-    grunt.registerTask('expose', ['localtunnel:server:keepalive']);
-    grunt.registerTask('default', ['compile', 'localtunnel', 'connect', 'watch']);
+    grunt.registerTask('expose', ['localtunnel_client:server:keepalive']);
+    grunt.registerTask('default', ['compile', 'localtunnel_client', 'connect', 'watch']);
 <% } else { %>
     grunt.registerTask('default', ['compile', 'connect', 'watch']);
 <% } %>
